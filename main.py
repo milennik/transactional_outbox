d1694d3411e3960b80b905f0ff00f5ebc3b1c846
Nikola <nikola.milenkovic@outlook.com>
2022-04-25 22:28:45 +0200

    feat(outbox): Add outbox model

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import json
-from models import applications
+from models import applications, outbox
 from app import app, db
+import uuid
 
 
 @app.route("/")
 def seed():
-    application = applications(name="nikola", age=35)
+    application = applications(id=uuid.uuid4(), name="nikola", age=35)
     db.session.add(application)
-    application = applications(name="jelena", age=30)
-    db.session.add(application)
+
+    ob = outbox(
+        domain="application",
+        domain_id=application.id,
+        type="app created",
+        payload=application.to_dict(),
+        status="Created",
+        try_count=0,
+    )
+    db.session.add(ob)
+
     db.session.commit()
+
     return (
         json.dumps({"success": True}),
         200,
--- models.py
+++ models.py
@@ -1,7 +1,24 @@
 from app import db
+import uuid
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy_serializer import SerializerMixin
 
 
-class applications(db.Model):
-    id = db.Column("application_id", db.Integer, primary_key=True)
+class CustomSerializerMixin(SerializerMixin):
+    serialize_types = ((UUID, lambda x: str(x)),)
+
+
+class applications(db.Model, CustomSerializerMixin):
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = db.Column(db.String(100))
     age = db.Column(db.INT)
+
+
+class outbox(db.Model):
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    domain = db.Column(db.String(100))
+    domain_id = db.Column(db.String(100))
+    type = db.Column(db.String(100))
+    payload = db.Column(db.JSON)
+    status = db.Column(db.String(50))
+    try_count = db.Column(db.Integer)
